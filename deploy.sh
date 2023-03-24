4890b141cce1c19d7765e624ddd84ba1f8a3aed4
denizirgin@gmail.com <denizirgin@gmail.com>
2023-03-24 15:33:48 +0300

    fix deploy.sh to login ECR

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 image_name=$1
+repo_path=$2
+
+# Authencticate to AWS ECR
+aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin $repo_path
 
 # Stop existing container
 docker rm $(docker ps -aq) -f || true
