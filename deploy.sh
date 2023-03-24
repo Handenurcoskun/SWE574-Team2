951c6213262d34aa477988130782108a15d3f2eb
denizirgin@gmail.com <denizirgin@gmail.com>
2023-03-24 16:31:04 +0300

    fix docker run wrong argument error

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
 image_name=$1
-repo_path=$2
-
-# Authencticate to AWS ECR
-aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin $repo_path
 
 # Stop existing container
 docker rm $(docker ps -aq) -f || true
@@ -13,4 +9,4 @@ docker rm $(docker ps -aq) -f || true
 docker pull $image_name
 
 # Run the latest container
-$ docker run -d --name my_space --restart always -p 80:80 $image_name -w /SWE573_Project $image_name sh -c "pip install -r requirements.txt && python manage.py runserver 0.0.0.0:80"
+docker run -d --name my_space --restart always -p 80:80 -w /SWE573_Project $image_name sh -c "pip install -r requirements.txt && python manage.py runserver 0.0.0.0:80"
