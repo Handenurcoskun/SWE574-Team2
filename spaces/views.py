f90421f276a1f03947f583a4a05e22d6fa813ddf
Hakan Balki <hakan.balki@boun.edu.tr>
2023-04-26 05:36:52 +0300

    correction to arrange roles capability to change other members roles

--- spaces/views.py
+++ spaces/views.py
@@ -100,42 +100,49 @@ def JoinSpaceView(request, pk):
     return HttpResponseRedirect(reverse('space-detail', args=[str(pk)]))
 
 # spaces/views.py
+# spaces/views.py
 class MembersListView(LoginRequiredMixin, UserPassesTestMixin, ListView):
     model = SpaceMembership
-    context_object_name = 'memberships'
     template_name = 'spaces/space_members.html'
-
-    def get_queryset(self):
-        space = get_object_or_404(Space, id=self.kwargs['pk'])
-        owner_membership = SpaceMembership(
-            user=space.owner,
-            space=space,
-            role='owner',
-        )
-        memberships = list(SpaceMembership.objects.filter(space=space))
-        memberships.insert(0, owner_membership)
-        return memberships
+    context_object_name = 'memberships'
 
     def test_func(self):
         space = get_object_or_404(Space, id=self.kwargs['pk'])
         return self.request.user == space.owner
 
+    def dispatch(self, request, *args, **kwargs):
+        if not self.test_func():
+            return self.handle_no_permission()
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_queryset(self):
+        space = get_object_or_404(Space, id=self.kwargs['pk'])
+        return SpaceMembership.objects.filter(space=space)
+
+
+
 
 class ChangeMemberRoleView(LoginRequiredMixin, UserPassesTestMixin, View):
-    def post(self, request, *args, **kwargs):
-        membership_id = self.kwargs['membership_id']
-        membership = get_object_or_404(SpaceMembership, id=membership_id)
-        new_role = request.POST.get('new_role')
-        membership.role = new_role
-        membership.save()
-        return redirect('members-list', membership.space.id)
 
     def test_func(self):
         space = get_object_or_404(Space, id=self.kwargs['pk'])
-        return self.request.user in space.members.all()
+        return self.request.user == space.owner
+
+    def post(self, request, *args, **kwargs):
+        space = get_object_or_404(Space, id=self.kwargs['pk'])
+        user_id = request.POST.get('user_id')
+        new_role = request.POST.get('new_role')
+
+        membership = get_object_or_404(SpaceMembership, space=space, user_id=user_id)
+        membership.role = new_role
+        membership.save()
+
+        return HttpResponseRedirect(reverse('members-list', args=[str(space.pk)]))
 
 
 
+
+# spaces/views.py
 # spaces/views.py
 class ModeratePostView(LoginRequiredMixin, UserPassesTestMixin, View):
 
