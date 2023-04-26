5fdc334f28b342274762ba56a063157fb619b01c
Hakan Balki <hakan.balki@boun.edu.tr>
2023-04-26 04:47:11 +0300

    correction to arrange roles capability to change other members roles

--- spaces/views.py
+++ spaces/views.py
@@ -118,10 +118,13 @@ class MembersListView(LoginRequiredMixin, UserPassesTestMixin, ListView):
 
     def test_func(self):
         space = get_object_or_404(Space, id=self.kwargs['pk'])
+        if self.request.user == space.owner:
+            return True
+
         memberships = SpaceMembership.objects.filter(space=space, user=self.request.user)
         if memberships.count() > 0:
             membership = memberships.first()
-            if membership.role == SpaceMembership.OWNER or space.policy == Space.PUBLIC:
+            if space.policy == Space.PUBLIC:
                 return True
         return False
 
