63441c03f39fb0205c348b2c30e42f718ac7586f
canal <canaltin96@gmail.com>
2023-05-07 23:59:41 +0300

    URL duplication: - Multiple link will be asked to creator - 2 option share or do not

--- blog/forms.py
+++ blog/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.forms import Textarea
-from .models import Comment
+from .models import Comment, Post
+
 
 class CommentForm(forms.ModelForm):
     class Meta:
@@ -9,3 +10,9 @@ class CommentForm(forms.ModelForm):
         widgets = {
             'content': Textarea(attrs={'rows': 3, 'cols': 40}),
         }
+
+
+class PostCreateUnderSpaceForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content', 'link', 'tags']
